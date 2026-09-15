1a45974162f226936a2efb40cb82dd1cf6fd5bd5
IamKS2999 <allprofessional.in@gmail.com>
2026-09-16 03:24:01 +0530

    Update brands.js

--- js/brands.js
+++ js/brands.js
@@ -1,1 +1,82 @@
+window.GCS = window.GCS || {};
 
+GCS.brands = {
+    Amazon: {
+        id: "Amazon",
+        name: "Amazon",
+        category: "Shopping",
+        logo: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Amazon_2024.svg",
+        fixedDiscount: 3,
+        customDiscount: 3,
+        fixedValues: [500, 1000, 2000, 5000],
+        customMin: 100,
+        customMax: 10000
+    },
+
+    Flipkart: {
+        id: "Flipkart",
+        name: "Flipkart",
+        category: "Shopping",
+        logo: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Flipkart_logo_(2026).svg",
+        fixedDiscount: 3.5,
+        customDiscount: 3.5,
+        fixedValues: [500, 1000, 2000, 5000],
+        customMin: 100,
+        customMax: 10000
+    },
+
+    Myntra: {
+        id: "Myntra",
+        name: "Myntra",
+        category: "Fashion",
+        logo: "https://commons.wikimedia.org/wiki/Special:Redirect/file/65c5da9f878952603e370d03_Myntra-Logo_1.svg",
+        fixedDiscount: 5,
+        customDiscount: 5,
+        fixedValues: [500, 1000, 2000, 5000],
+        customMin: 100,
+        customMax: 10000
+    },
+
+    Croma: {
+        id: "Croma",
+        name: "Croma",
+        category: "Electronics",
+        logo: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Croma_logo.png",
+        fixedDiscount: 5,
+        customDiscount: 5,
+        fixedValues: [500, 1000, 2000, 5000],
+        customMin: 100,
+        customMax: 10000
+    },
+
+    Domino: {
+        id: "Domino",
+        name: "Domino's",
+        category: "Food",
+        logo: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Domino%27s_pizza_logo.svg",
+        fixedDiscount: 15,
+        customDiscount: 5,
+        fixedValues: [500, 1000, 2000, 5000],
+        customMin: 100,
+        customMax: 10000
+    }
+};
+
+GCS.getBrand = function(id) {
+    return GCS.brands[id] || null;
+};
+
+GCS.calculatePrice = function(amount, discount) {
+    amount = Number(amount);
+    discount = Number(discount);
+
+    if (!Number.isFinite(amount) || amount <= 0) {
+        return 0;
+    }
+
+    if (!Number.isFinite(discount)) {
+        discount = 0;
+    }
+
+    return Math.round(amount * (1 - discount / 100));
+};
