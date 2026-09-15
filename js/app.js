b6e246abbb172eda9f88c93bfc41ca8ddb236ba8
IamKS2999 <allprofessional.in@gmail.com>
2026-09-16 03:26:19 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+window.GCS = window.GCS || {};
 
+document.addEventListener("DOMContentLoaded", function() {
+
+    GCS.renderProducts();
+    GCS.updateHeader();
+
+    document.getElementById("loginButton")
+        .addEventListener("click", GCS.openLogin);
+
+    document.getElementById("accountButton")
+        .addEventListener("click", GCS.openAccount);
+
+    document.getElementById("search")
+        .addEventListener("input", GCS.searchCards);
+
+    document.querySelectorAll(".category")
+        .forEach(button => {
+            button.addEventListener("click", function() {
+                GCS.filterCategory(
+                    button.textContent.trim(),
+                    button
+                );
+            });
+        });
+
+});
