cb830af32a2bf75b724c39a742c7facffa53312b
IamKS2999 <allprofessional.in@gmail.com>
2026-09-16 16:14:36 +0530

    Update catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -1,105 +1,62 @@
 /* =====================================================
    GIFTCARDSTORE — CATALOG
-   LOGO DATABASE
-   VERSION: 2026-09-16-14
+   PASSIVE CATALOG LOADER
+   VERSION: 2026-09-16-15
 ===================================================== */
 
 (function () {
 
     "use strict";
 
-    const LOGOS = {
+    /*
+     * IMPORTANT:
+     * catalog.js must NOT modify BRANDS.logo.
+     *
+     * Logo information belongs to brands.js.
+     * This file only refreshes images that the
+     * application has already assigned.
+     */
 
-        /* =========================
-           SHOPPING
-        ========================= */
+    function refreshImages() {
 
-        "Amazon":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Amazon_2024.svg",
+        document
+            .querySelectorAll(
+                "img.brand-logo, #productLogo, .owned-gift-logo img"
+            )
+            .forEach(function (img) {
 
-        "Flipkart":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Flipkart_logo_(2026).svg",
+                if (!img.src || img.src === window.location.href) {
+                    return;
+                }
 
-        "Tata CLiQ":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/TATA_Cliq_Logo.jpg",
+                img.loading = "eager";
 
-        "Meesho":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Meesho_logo.png",
+            });
 
-
-        /* =========================
-           FASHION
-        ========================= */
-
-        "Myntra":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/65c5da9f878952603e370d03_Myntra-Logo_1.svg",
-
-        "AJIO":
-            "https://www.svgrepo.com/download/504079/ajio.svg",
-
-        "Max Fashion":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Logo_of_Max_Fashion_and_Accessories,_March_2018.png",
-
-        "Shoppers Stop":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Shoppers_Stop_Logo.gif",
-
-        "Levi's":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Levi%27s_logo.svg",
-
-        "Decathlon":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Decathlon_Logo24.svg",
-
-        "FirstCry":
-            "https://www.svgrepo.com/download/504335/firstcry.svg",
-
-        "Marks & Spencer":
-            "https://cdn.simpleicons.org/marksandspencer/000000",
-
-        "Van Heusen":
-            "https://cdn.simpleicons.org/vanheusen/000000",
-
-        "Allen Solly":
-            "https://cdn.simpleicons.org/allensolly/000000",
-
-        "Peter England":
-            "https://cdn.simpleicons.org/peterengland/003B5C",
+    }
 
 
-        /* =========================
-           ELECTRONICS / HOME
-        ========================= */
+    function initialise() {
 
-        "Croma":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Croma_logo.png",
+        refreshImages();
 
-        "Reliance Digital":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Reliance_Digital.svg",
+        setTimeout(refreshImages, 500);
+        setTimeout(refreshImages, 1500);
 
-        "Vijay Sales":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/VijaySale-Logo.png",
-
-        "IKEA":
-            "https://cdn.simpleicons.org/ikea/0058A3",
-
-        "Pepperfry":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Pepperfry_logo.jpg",
+    }
 
 
-        /* =========================
-           FOOD
-        ========================= */
+    if (document.readyState === "loading") {
 
-        "Domino's":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Domino%27s_2025.svg",
+        document.addEventListener(
+            "DOMContentLoaded",
+            initialise
+        );
 
-        "Zomato":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Zomato_Logo.svg",
+    } else {
 
-        "Swiggy":
-            "https://commons.wikimedia.org/wiki/Special:Redirect/file/Swiggy_logo.png",
+        initialise();
 
-        "Haldiram's":
-            "https://cdn.simpleicons.org/haldirams/E31B23",
+    }
 
-
-        /*
+})();
